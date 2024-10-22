e51cc136fdfa590471af5a15ad0ae84ba2a3c82a
Diego-oli <diegodutradeoliveira0@gmail.com>
2024-10-22 17:15:40 -0300

    mapa
    melhorar mapa

---
+++ mapa/mapa.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const startButton = document.querySelector('.start-button button');
+    startButton.addEventListener('click', () => {
+        alert('Jogo Iniciado!');
+    });
+});
